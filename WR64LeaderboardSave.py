3a9334cd992634f1b097bd98af7b247ad39c1e88
Scott Olm <17048085+MonotoneCulprit@users.noreply.github.com>
2026-07-31 16:13:10 -0400

    Update WR64LeaderboardSave.py

--- WR64LeaderboardSave.py
+++ WR64LeaderboardSave.py
@@ -8,127 +8,109 @@ import srcomapi.datatypes as dt
 import requests
 import json
 import pandas as pd
+import ids
+import splits
+import savemanipulation as sm
 
 api = srcomapi.SpeedrunCom()
 
-SB_id = "ldypjjd3"
-SBAY_id = "gdrqg89z"
-DL_id = "nwl7np9v"
-MF_id = "ywe8nqwl"
-PB_id = "69z4x6w1"
-TC_id = "r9g3rqw2"
-GC_id = "o9xlk69l"
-SI_id = "4958y29p"
+# Get 3 Lap top 3 data for each level
+SB3data = sm.gettopthree(ids.SB_id, ids.ThreeL_id)
+SBay3data = sm.gettopthree(ids.SBay_id, ids.ThreeL_id)
+DL3data = sm.gettopthree(ids.DL_id, ids.ThreeL_id)
+MF3data = sm.gettopthree(ids.MF_id, ids.ThreeL_id)
+PB3data = sm.gettopthree(ids.PB_id, ids.ThreeL_id)
+TC3data = sm.gettopthree(ids.TC_id, ids.ThreeL_id)
+GC3data = sm.gettopthree(ids.GC_id, ids.ThreeL_id)
+SI3data = sm.gettopthree(ids.SI_id, ids.ThreeL_id)
 
-ThreeL_id = "02qpq7dy"
-OneL_id = "ndx7yo2q"
-Reverse_id = "w20v0jkn"
+# Get 1 Lap World Record data for each level
+SB1data = sm.gettoprun(ids.SB_id)
+SBay1data = sm.gettoprun(ids.SBay_id)
+DL1data = sm.gettoprun(ids.DL_id)
+MF1data = sm.gettoprun(ids.MF_id)
+PB1data = sm.gettoprun(ids.PB_id)
+TC1data = sm.gettoprun(ids.TC_id)
+GC1data = sm.gettoprun(ids.GC_id)
+SI1data = sm.gettoprun(ids.SI_id)
 
-def gettopthree (level_id, category_id):
-    requesturl = f"https://www.speedrun.com/api/v1/leaderboards/wr64/level/{level_id}/{category_id}?var-p85901vn=rqv42owq&top=3"
+# Get Reverse World Record data for each level
+SBRdata = sm.gettopthree(ids.SB_id, ids.Reverse_id)
+SBayRdata = sm.gettopthree(ids.SBay_id, ids.Reverse_id)
+DLRdata = sm.gettopthree(ids.DL_id, ids.Reverse_id)
+MFRdata = sm.gettopthree(ids.MF_id, ids.Reverse_id)
+PBRdata = sm.gettopthree(ids.PB_id, ids.Reverse_id)
+TCRdata = sm.gettopthree(ids.TC_id, ids.Reverse_id)
+GCRdata = sm.gettopthree(ids.GC_id, ids.Reverse_id)
+SIRdata = sm.gettopthree(ids.SI_id, ids.Reverse_id)
 
-    response = requests.get(requesturl).json()["data"]["runs"]
-    data = response
-    keys = "place", "id", "primary_t"
+# Isolate 3 Lap times and names into individual variables for each level
+SB3firsttime, SB3secondtime, SB3thirdtime = sm.extracttimes_mariner(SB3data)
+SB3firstname, SB3secondname, SB3thirdname = sm.extractnames(SB3data)
+SBay3firsttime, SBay3secondtime, SBay3thirdtime = sm.extracttimes_mariner(SBay3data)
+SBay3firstname, SBay3secondname, SBay3thirdname = sm.extractnames(SBay3data)
+DL3firsttime, DL3secondtime, DL3thirdtime = sm.extracttimes_mariner(DL3data)
+DL3firstname, DL3secondname, DL3thirdname = sm.extractnames(DL3data)
+MF3firsttime, MF3secondtime, MF3thirdtime = sm.extracttimes_jeter(MF3data)
+MF3firstname, MF3secondname, MF3thirdname = sm.extractnames(MF3data)
+PB3firsttime, PB3secondtime, PB3thirdtime = sm.extracttimes_jeter(PB3data)
+PB3firstname, PB3secondname, PB3thirdname = sm.extractnames(PB3data)
+TC3firsttime, TC3secondtime, TC3thirdtime = sm.extracttimes_mariner(TC3data)
+TC3firstname, TC3secondname, TC3thirdname = sm.extractnames(TC3data)
+GC3firsttime, GC3secondtime, GC3thirdtime = sm.extracttimes_mariner(GC3data)
+GC3firstname, GC3secondname, GC3thirdname = sm.extractnames(GC3data)
+SI3firsttime, SI3secondtime, SI3thirdtime = sm.extracttimes_mariner(SI3data)
+SI3firstname, SI3secondname, SI3thirdname = sm.extractnames(SI3data)
 
-    data = filterleaderboard(data, keys)
+# Isolate 1 Lap times and names into individual variables for each level
+SB1time = sm.extracttime_mariner(SB1data)
+SB1name = SB1data.Player[0]
+SBay1time = sm.extracttime_mariner(SBay1data)
+SBay1name = SBay1data.Player[0]
+DL1time = sm.extracttime_mariner(DL1data)
+DL1name = DL1data.Player[0]
+MF1time = sm.extracttime_jeter(MF1data)
+MF1name = MF1data.Player[0]
+PB1time = sm.extracttime_jeter(PB1data)
+PB1name = PB1data.Player[0]
+TC1time = sm.extracttime_mariner(TC1data)
+TC1name = TC1data.Player[0]
+GC1time = sm.extracttime_mariner(GC1data)
+GC1name = GC1data.Player[0]
+SI1time = sm.extracttime_mariner(SI1data)
+SI1name = SI1data.Player[0]
 
-    data = pd.json_normalize(data)
-    data = filterleaderboard(data, keys)
-    del data["run.id"]
-    del data["run.videos.links"]
-    data['run.players'] = data['run.players'].apply(
-        lambda x: x[0]['id'] if isinstance(x, list) and len(x) > 0 else x
-    )
+# Isolate Reverse time and name into individual variables for each level
+SBRtime = sm.extracttime_mariner(SBRdata)
+SBRname = SBRdata.Player[0]
+SBayRtime = sm.extracttime_mariner(SBayRdata)
+SBayRname = SBayRdata.Player[0]
+DLRtime = sm.extracttime_mariner(DLRdata)
+DLRname = DLRdata.Player[0]
+MFRtime = sm.extracttime_hayami(MFRdata)
+MFRname = MFRdata.Player[0]
+PBRtime = sm.extracttime_jeter(PBRdata)
+PBRname = PBRdata.Player[0]
+TCRtime = sm.extracttime_mariner(TCRdata)
+TCRname = TCRdata.Player[0]
+GCRtime = sm.extracttime_mariner(GCRdata)
+GCRname = GCRdata.Player[0]
+SIRtime = sm.extracttime_mariner(SIRdata)
+SIRname = SIRdata.Player[0]
 
-    data = data.rename(
-        columns={
-            "run.times.primary_t": "Time",
-            "run.players": "Player",
-            "place": "Place"
-            })
-    
-    for i in range(3):
-        data.at[i, "Player"] = getplayername(data.at[i, "Player"])
-        
-    return data
+# Start building the save file
 
-def gettoprun (level_id):
-    requesturl = f"https://www.speedrun.com/api/v1/leaderboards/wr64/level/{level_id}/{OneL_id}?var-p85901vn=rqv42owq&top=1"
+# Write Header
+sm.writetooffset(bytes.fromhex('5445'), 0x0)
 
-    response = requests.get(requesturl).json()["data"]["runs"]
-    data = response
-    keys = "place", "id", "primary_t"
+# Write FF Spaces
+sm.writetooffset(bytes.fromhex('FFFFFFFF'), 0x4)
 
-    data = filterleaderboard(data, keys)
+# Unlock all tracks and difficulties
+sm.writetooffset(bytes.fromhex('06060707'), 0x8)
 
-    data = pd.json_normalize(data)
-    data = filterleaderboard(data, keys)
-    del data["run.id"]
-    del data["run.videos.links"]
-    data['run.players'] = data['run.players'].apply(
-        lambda x: x[0]['id'] if isinstance(x, list) and len(x) > 0 else x
-    )
-    
+# Unlock all Championships and set Audio to Stereo with Music On
+sm.writetooffset(bytes.fromhex('0F'), 0xC)
 
-    data = data.rename(
-        columns={
-            "run.times.primary_t": "Time",
-            "run.players": "Player",
-            "place": "Place"
-            })
-    
-    data.at[0, "Player"] = getplayername(data.at[0, "Player"])
-    
-    return data
-
-def getplayername (player_id):
-    url = f"https://www.speedrun.com/api/v1/users/{player_id}"
-    
-    response = requests.get(url)
-    data = response.json()
-    username = data["data"]["names"]["international"]
-    return username
-
-def filterleaderboard(data, keep_keys):
-    if isinstance(data, dict):
-        # Filter the dictionary keys and recursively filter their values
-        return {
-            key: filterleaderboard(value, keep_keys)
-            for key, value in data.items()
-            if key in keep_keys or isinstance(value, (dict, list))
-        }
-    elif isinstance(data, list):
-        # Recursively filter each item in the list
-        return [filterleaderboard(item, keep_keys) for item in data]
-    else:
-        # Return primitive values (strings, numbers, booleans, None) as-is
-        return data
-    
-    
-SB3data = gettopthree(SB_id, ThreeL_id)
-SBAY3data = gettopthree(SBAY_id, ThreeL_id)
-DL3data = gettopthree(DL_id, ThreeL_id)
-MF3data = gettopthree(MF_id, ThreeL_id)
-PB3data = gettopthree(PB_id, ThreeL_id)
-TC3data = gettopthree(TC_id, ThreeL_id)
-GC3data = gettopthree(GC_id, ThreeL_id)
-SI3data = gettopthree(SI_id, ThreeL_id)
-
-SB1data = gettoprun(SB_id)
-SBAY1data = gettoprun(SBAY_id)
-DL1data = gettoprun(DL_id)
-MF1data = gettoprun(MF_id)
-PB1data = gettoprun(PB_id)
-TC1data = gettoprun(TC_id)
-GC1data = gettoprun(GC_id)
-SI1data = gettoprun(SI_id)
-
-SBRdata = gettopthree(SB_id, Reverse_id)
-SBAYRdata = gettopthree(SBAY_id, Reverse_id)
-DLRdata = gettopthree(DL_id, Reverse_id)
-MFRdata = gettopthree(MF_id, Reverse_id)
-PBRdata = gettopthree(PB_id, Reverse_id)
-TCRdata = gettopthree(TC_id, Reverse_id)
-GCRdata = gettopthree(GC_id, Reverse_id)
-SIRdata = gettopthree(SI_id, Reverse_id)
+# Write FF Spaces
+sm.writetooffset(bytes.fromhex('FFFFFF'), 0xD)
